c8dcea94a8b4e1ba257fe0c7b8a5b5c0ae504188
Igor Shlyakhtenkov <ishlyakhtenkov@yandex.ru>
2025-05-22 09:58:06 +0300

    init commit

---
+++ src/main/java/ru/javaprojects/picnest/PicnestApplication.java
@@ -0,0 +1,13 @@
+package ru.javaprojects.picnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicnestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicnestApplication.class, args);
+	}
+
+}
